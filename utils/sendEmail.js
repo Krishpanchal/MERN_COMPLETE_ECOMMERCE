8909b99803feedf2be604e6b9c98e6403bb3f838
Krishpanchal <63506149+Krishpanchal@users.noreply.github.com>
2021-09-26 16:30:34 +0530

    Added gmail service for reset password

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,17 +1,42 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+  let transporter;
+  let emailFrom;
+
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "DEVELOPMENT"
+  ) {
+    emailFrom = process.env.SMTP_EMAIL_FROM;
+
+    transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+  }
+
+  if (
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "PRODUCTION"
+  ) {
+    emailFrom = process.env.EMAIL_FROM;
+
+    transporter = nodemailer.createTransport({
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
   const mailOptions = {
-    from: process.env.SMTP_EMAIL_FROM,
+    from: emailFrom,
     to: options.to,
     subject: options.subject,
     html: options.message,
